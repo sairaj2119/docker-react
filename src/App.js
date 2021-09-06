448ed543f25ccdc0899ac9282e95d77c69858062
sairaj <sairaj2119@gmail.com>
2021-09-06 11:56:38 +0530

    done with docker

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi how are you
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 2.0
         </a>
       </header>
     </div>
